2f868dac0ffa33aa78ce505c082c16a12683b926
Joel Wasserman <joel.wasserman8@gmail.com>
2018-08-31 13:36:33 -0700

    update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import pig3 from '../images/pig3.jpeg'
 import '../styles/App.css'
 
 class Contact extends Component {
@@ -7,13 +8,14 @@ class Contact extends Component {
       <div className="App">
         <div className="App-body">
           <h3>Contact Me</h3>
-          <div className="BookSummary">
-            <div className="AuthorText">
+          <div className="ContactSummary">
+            <div className="ContactText">
               <p>I hope you enjoyed the book!</p>
               <p>I welcome any comments, questions, or concerns that people
-have about this book, Halloween, or pigs in their lives</p>
+have about this book, Halloween, or pigs in their lives.</p>
               <p>Email me at: <a href="emailto:pigaboobooks@gmail.com">pigaboobooks@gmail.com</a></p>
             </div>
+            <img className="PeekingPigContactLeft" alt="Peeking Pig" src={pig3}/>
           </div>
         </div>
       </div>
